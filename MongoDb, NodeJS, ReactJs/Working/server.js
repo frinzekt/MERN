46afbc1a2bdcd42a556345b10c1fb94723f5c7c9
frinzekt <frinzekt@gmail.com>
2019-07-22 00:26:58 +0800

    MRN 02_04 EJS
    EJS is a template language used to create dynamic websites by having ejs tags that creates dynamic html code used to display sets of values such as imports of other ejs tags, javascript codes and more

--- MongoDb, NodeJS, ReactJs/Working/server.js
+++ MongoDb, NodeJS, ReactJs/Working/server.js
@@ -10,23 +10,19 @@ It can also handle server-side routing that can expose API
 to listen to certain routes eg. server.get
 */
 
+server.set("view engine", "ejs");
+
 server.get("/", (req, res) => {
-	//Two parameters: path server for "Req and Res", and request and response routes
-	res.send("Hello Express");
+	//To render EJS component omitting the extension as default
+	//Render has two arguments, path and variable passing
+	res.render("index", {
+		content: "Hello This is Variable passing of <em>var content</em>"
+	});
 });
 
 server.use("/api", apiRouter);
 //Express has a static middleware which serves static files easily
 server.use(express.static("public"));
-
-/*
-server.get("/about.html", (req, res) => {
-	//res.send("This is about page");
-	fs.readFile("./about.html", (err, data) => {
-		//Reads the data of the file and turns it into a string for sending
-		res.send(data.toString());
-	});
-});*/
 
 server.listen(config.port, () => {
 	//Success Handler on CallBack Function
