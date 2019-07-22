3a239ef2b096fe7abd76be81190eb50fd1b1789e
frinzekt <frinzekt@gmail.com>
2019-07-22 11:43:44 +0800

    MRN 03_02 React Componente

--- MongoDb, NodeJS, ReactJs/Working/src/index.js
+++ MongoDb, NodeJS, ReactJs/Working/src/index.js
@@ -1,11 +1,24 @@
 import React from "react";
+import PropTypes from "prop-types";
 import ReactDom from "react-dom";
 
 const color = Math.random() > 0.5 ? "green" : "red";
 
-ReactDom.render(
-	<h2 className="text-center" style={{ color }}>
-		JSX -- {Math.random()}
-	</h2>,
-	document.getElementById("root")
-);
+const App = props => {
+	return (
+		<h2 className="text-center" style={{ color }}>
+			{props.headerMessage}
+		</h2>
+	);
+};
+
+App.propTypes = {
+	//string refers to limitation of type via string, isRequired refers to limitation of non-null value
+	headerMessage: PropTypes.string.isRequired
+};
+
+App.defaultProps = {
+	headerMessage: "Hello"
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
